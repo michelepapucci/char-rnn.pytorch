72375df38cc06fe58d0d89fb139f382ba386efea
Michele <m.papucci@studenti.unipi.it>
2023-05-10 12:29:18 +0200

    fixing cuda error

--- generate.py
+++ generate.py
@@ -8,13 +8,15 @@ import argparse
 from helpers import *
 from model import *
 
-def generate(decoder, prime_str='A', predict_len=100, temperature=0.8, stop_char="\n", cuda=False):
+def generate(decoder, prime_str='A', predict_len=100, temperature=0.8, stop_char="\n", use_cuda=False):
     hidden = decoder.init_hidden(1)
     prime_input = Variable(char_tensor(prime_str).unsqueeze(0))
 
-    if cuda:
-        hidden = hidden.cuda()
+    if use_cuda:
+        for h in hidden:
+            hidden = tuple(h.cuda() for h in hidden)
         prime_input = prime_input.cuda()
+        
     predicted = prime_str
 
     # Use priming string to "build up" hidden state
